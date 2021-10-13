123488a1e72564a558e7c8a0a9db41bf9b7b10a3
Angelina <karangelina@icloud.com>
2021-10-13 10:29:49 +0800

    Lesson01

--- Lesson1/2.py
+++ Lesson1/2.py
@@ -4,5 +4,14 @@ hours = seconds // 3600
 minutes = seconds // 60 - hours * 60
 seconds = seconds - hours * 3600 - minutes * 60
 if hours < 10:
-    hours = int(f"{0}{hours}")
-print(hours, minutes, seconds)
+    print("0", hours, end=":", sep="")
+else:
+    print(hours, end=":")
+if minutes < 10:
+    print("0", minutes, end=":", sep="")
+else:
+    print(minutes, end=":")
+if seconds < 10:
+    print("0", seconds, sep="")
+else:
+    print(seconds)
